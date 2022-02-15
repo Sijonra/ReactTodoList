b3a5ea293f5a38d1b823b875c731f50f9f936f6f
Ilya <sijonra@gmail.com>
2022-02-15 15:00:35 +0300

    first commit

---
+++ src/TodoList/todoList.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+
+function todoList(){
+    return(
+        <ul className="todoList">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+export default todoList;
